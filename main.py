06524503cc28ff9c6ec1df474d1becedf2b81047
zzung00 <lee759444@gmail.com>
2022-06-11 16:24:31 +0900

    Feat: Added delivery departure

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ def on_message(client, userdata, msg):
     
     m_decode = str(msg.payload.decode("utf-8", "ignore"))
     deliveryRequest = json.loads(str(m_decode))
-    print(msg.topic + " " + deliveryRequest['username'] + "\n" + deliveryRequest['destination'] )
+    print(msg.topic + " " + deliveryRequest['source'] + "\n" + deliveryRequest['username'] + "\n" + deliveryRequest['destination'] )
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("localhost", 1883, 60)
+client.connect("0.0.0.0", 1883, 60)
 client.loop_forever()
